2a1034d7af064e5754f04691b68430379d868e13
Abhishek Kumar <abhishek.kumar.ak@gmail.com>
2013-09-25 22:14:14 -0700

    Refactored

--- src/parameters.h
+++ src/parameters.h
@@ -1,49 +1,48 @@
-
 #define PI 3.141592654
 
 /*
-	Gaussian Distribution sampling
-	Courtesy http://c-faq.com/lib/gaussian.html
-	I've chosen an implementation that is not the fastest, but is more accurate 
+  Gaussian Distribution sampling
+  Courtesy http://c-faq.com/lib/gaussian.html
+  I've chosen an implementation that is not the fastest, but is more accurate
 */
 double gaussrand();
 
 typedef struct sparameters{
-	floatnumber * parametervector;
-	const floatnumber * cparametervector;
-	int p,d,k,layer1N,layer2N,layer3N,N;
+  floatnumber * parametervector;
+  const floatnumber * cparametervector;
+  int p,d,k,layer1N,layer2N,layer3N,N;
 
-	// p,d,k are self explanatory
-	// initializevector = 1 means vector will be initialized randomly
-	//                      with appropriate gaussian 
-	// initializevector = 0 meanse vector will set to all zero
-	sparameters(int pp, int dd, int kk, bool initializevector = false);
+  // p,d,k are self explanatory
+  // initializevector = 1 means vector will be initialized randomly
+  //                      with appropriate gaussian
+  // initializevector = 0 meanse vector will set to all zero
+  sparameters(int pp, int dd, int kk, bool initializevector = false);
 
-	// Initialize from a previous set of values
-	sparameters(int pp, int dd, int kk, const floatnumber *w);
+  // Initialize from a previous set of values
+  sparameters(int pp, int dd, int kk, const floatnumber *w);
 
-	floatnumber & val(int layer, int inputidx, int outputidx);
+  floatnumber & val(int layer, int inputidx, int outputidx);
 
-	floatnumber val(int layer, int inputidx, int outputidx) const;
+  floatnumber val(int layer, int inputidx, int outputidx) const;
 
-	floatnumber & operator[](const int idx);
+  floatnumber & operator[](const int idx);
 
-	floatnumber operator[](const int idx) const;
+  floatnumber operator[](const int idx) const;
 
-	floatnumber * getvector();
+  floatnumber * getvector();
 
-	const floatnumber * getvector() const;
+  const floatnumber * getvector() const;
 
 
-	// Should be called by the caller of init()
-	// Except when initialized from previous set of values
-	void destroy();
+  // Should be called by the caller of init()
+  // Except when initialized from previous set of values
+  void destroy();
 
 } parameters;
 
 bool comparator ( const std::pair<floatnumber, int>& l, const std::pair<floatnumber, int>& r);
 
-/* 
+/*
  * Prints the top tags for each hidden node
  * p: parameter structure
  * n: Number of tags to print for each hidden node
